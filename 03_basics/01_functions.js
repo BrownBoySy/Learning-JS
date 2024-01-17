8aa630339b37d9ad284b56df428fcc6d34063b04
Satyam Sharma <155839810+BrownBoySy@users.noreply.github.com>
2024-01-17 18:14:14 +0000

    global and local scopes

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -34,4 +34,33 @@ function loginUserMessage(username){
 }
 
 // console.log(loginUserMessage("Satyam"));
-console.log(loginUserMessage());
+// console.log(loginUserMessage());
+
+
+function calculateCartPrice(...num1){
+    return num1
+}
+// console.log(calculateCartPrice(200, 300, 500, 1000));
+
+const user = {
+    username: "Satyam",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+username: "Sam",
+price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500]));
